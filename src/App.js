03895e044b546cc9a269881f63d82f7073038aee
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-23 19:17:21 -0300

    Começo do projeto, com configuração básica

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./css/app.css"
+import "./css/bootstrap-utilities.css"
+import "./css/pico-bootstrap-grid.css";
+
+function App() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
