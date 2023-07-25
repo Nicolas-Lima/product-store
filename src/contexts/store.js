af42a291eb3cb2056fd4e3720e66c8469271a6ae
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-24 21:32:20 -0300

    Contexto store atualizado

--- src/contexts/store.js
+++ src/contexts/store.js
@@ -1,9 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const StoreContext = createContext({});
 
 function StoreProvider({ children }) {
-  const contextValue = {};
+  const [products, setProducts] = useState([
+    {
+      name: "óculos de sol",
+      imgUrl: "https://aa.com",
+      type: "Beleza",
+      price: 10,
+      seller: "A & B",
+      minPurchaseUnits: 1,
+      maxPurchaseUnits: 3,
+      stock: "inglês",
+    },
+  ]);
+
+  useEffect(() => {}, []);
+
+  const contextValue = { products, setProducts };
+
   return (
     <StoreContext.Provider value={contextValue}>
       {children}
